78a9cff115750e909f4f85f9ccdd4590ae1f9d56
mhdanh <mhdanh@aavn-ws-156.aavn.local>
2014-11-07 17:15:02 +0700

    init exportpdf

--- exportexcel/src/main/java/com/mhdanh/controller/IndexController.java
+++ exportexcel/src/main/java/com/mhdanh/controller/IndexController.java
@@ -1,15 +1,17 @@
 package com.mhdanh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class WebserviceController {
-	
-	@RequestMapping("/ws/hello")
+public class IndexController {
+
+	@RequestMapping("/index")
 	@ResponseBody
-	public String hello(){
-		return "hello resttemplate";
+	public String index(){
+		return "ok man";
 	}
+	
 }
